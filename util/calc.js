b46d6cfa03c005341b2172fa23a920ff0fdeed92
Hoff <haffedmcnair@gmail.com>
2019-06-10 11:32:43 -0700

    building a type matrix for fast/lighter type checking

--- util/calc.js
+++ util/calc.js
@@ -1,7 +1,8 @@
 const moveList = require("../db/moves")
 const Pokemon = require("../classes/pokemon")
 function calc(mon1, mon2, move1, move2){
-    let stab,typeMod, arbitraryNum
+    let stab,typeMod,arbitraryNumm,moveType
+    moveType = moveList[move1].type
     // Damage calc pre STAB, 
     let damage = (((1/5+2)*mon1.stats.atk*moveList[move1].basePower)/mon2.stats.def)
     damage = damage/50
@@ -9,13 +10,15 @@ function calc(mon1, mon2, move1, move2){
 
 
     //STAB goes here, either 1.5 or 1, for this test we will use 1.5
-    if (moveList[move1].type == mon1.type[0] || moveList[move1].type == mon1.type[1]){
+    if (moveType== mon1.type[0] || moveType== mon1.type[1]){
         stab = 1.5
     } else{
         stab = 1
     }
+
     
     damage = damage * stab;
+
     // Next is type modifier
     damage = damage * 2
 
