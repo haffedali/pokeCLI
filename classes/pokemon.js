cd7e639b47624123c6cae3685ac5d22d72116971
Austin <Austin.shepherd.96@gmail.com>
2019-06-12 11:31:16 -0700

    added some stuff

--- classes/pokemon.js
+++ classes/pokemon.js
@@ -8,7 +8,7 @@ class Pokemon {
         this.type = pokemon[name].types
         this.stats = pokemon[name].baseStats
         this.moves = pokemon[name].moveSet
-        this.status = "normal";
+        this.status = null;
         // this.moveList = pokemon[name].moveList 
         this.moves = moves;
         //JUST A BLOCK FOR THE HEALTH CALC, DONT WANT IT GETTING TOO MESSY
@@ -16,24 +16,9 @@ class Pokemon {
         this.health = (((2*pokemon[name].baseStats.hp + 30 + 20) * 78)/100) + 110
     }
 
-
-    printMoves() {
-        for (let i = 0; i < this.moves.length; i++) {
-            console.log(this.moves[i])
-        }
-    }
-
-    printStats() {
-        console.log(this.stats)
-    }
-
-    takeDamage(damage){//must be a number, generrated form
-        this.stats.hp -= damage
-    }
-    useAttack(attackName, target){//must be a string References
-        //will insert thick calculations later, this is just Proof of Concept 
-        let dmg = moves[attackName].basePower * 0.15 //<shitty calc that makes no sense, haffed help
-        target.takeDamage(Math.floor(dmg)) 
+    takeDamage(damage, status = null){//must be a number, generrated form
+        this.status = status
+        this.health -= damage
     }
 
 }
