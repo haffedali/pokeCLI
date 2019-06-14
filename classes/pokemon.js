902a7f78189985af8769648c5a73d079b2a3325e
Hoff <haffedmcnair@gmail.com>
2019-06-13 21:17:21 -0700

    fixed moves

--- classes/pokemon.js
+++ classes/pokemon.js
@@ -10,7 +10,7 @@ class Pokemon {
         this.moves = pokemon[name].moveSet
         this.status = null;
         // this.moveList = pokemon[name].moveList 
-        this.moves = moves;
+        // this.moves = moves;
         //JUST A BLOCK FOR THE HEALTH CALC, DONT WANT IT GETTING TOO MESSY
         //For now, assuming perfect IV, max level, and decent EV's
         this.health = Math.floor((((2*pokemon[name].baseStats.hp + 30 + 20) * 78)/100) + 110)
